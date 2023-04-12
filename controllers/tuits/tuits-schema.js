aa443f62d6517b0ed68e6a4bd7114f9ce7d0855c
王梦如 <mujojo@ounoMacBook-Air.local>
2023-04-12 14:57:12 -0700

    first commit

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
-liked: Boolean,
+    liked: Boolean,
 }, {collection: 'tuits'}); 
 export default schema;
